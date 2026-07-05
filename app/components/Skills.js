e6118899f0bd259c379d595f8f5b408deeb51aec
Adithep <67319100052@panitthon.ac.th>
2026-07-05 17:38:56 +0700

    deploy

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -12,14 +12,14 @@ export default function Skills() {
         <span className="h-4 w-1.5 rounded-full bg-blue-600"></span>
         <h2 className="text-lg font-black text-slate-900 tracking-tight">ทางลัดระบบ (Quick Links)</h2>
       </div>
-      
+
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {links.map((item, index) => (
-          <a 
-            href={item.url} 
-            key={index} 
+          <a
+            href={item.url}
+            key={index}
             target={item.url.startsWith('http') ? "_blank" : "_self"}
-            rel="noopener noreferrer" 
+            rel="noopener noreferrer"
             className="p-6 bg-white rounded-2xl shadow-[0_4px_20px_-4px_rgba(0,0,0,0.03)] border border-slate-100 hover:border-blue-500 hover:shadow-xl hover:shadow-blue-500/5 text-center transition duration-300 block group transform hover:-translate-y-1"
           >
             <div className="text-4xl mb-3 transform group-hover:scale-110 transition duration-300 inline-block">
